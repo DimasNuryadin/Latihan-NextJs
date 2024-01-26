3588f02492b862b56525c98884b404983a77f741
DimasNuryadin <dimasnuryadinn@gmail.com>
2024-01-26 22:28:53 +0700

    create custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <div>
+      <h1 className="title-not-found">Oooops...</h1>
+      <h1 className="title-not-found">Halaman yang anda cari tidak ada</h1>
+    </div>
+  )
+}
