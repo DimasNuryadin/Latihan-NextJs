cb688cd44271c5b7a379958372e9217125396c0e
DimasNuryadin <dimasnuryadinn@gmail.com>
2024-01-27 17:34:35 +0700

    try image component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import Layout from "@/components/Layout";
 import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <Layout pageTitle="Homepage">
+      <Image src="/Profile.png" width={200} height={200} alt="Profile" />
+      <img src="/Profile.png" style={{ width: 200, height: 200 }} alt="Profile" />
       <h1 className={styles['title-homepage']}>Welcome Dimas</h1>
     </Layout>
   );
