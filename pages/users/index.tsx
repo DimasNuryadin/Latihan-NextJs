7933a5e8cc9eabb2d252cf40c2f767823a8f34fd
DimasNuryadin <dimasnuryadinn@gmail.com>
2024-01-30 19:42:57 +0700

    create data fetching static props

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,9 +1,36 @@
 import Layout from "@/components/Layout";
 
-export default function UsersPage() {
+interface UsersProps {
+  readonly dataUsers: Array<any>;
+}
+
+export default function UsersPage(props: UsersProps) {
+  const { dataUsers } = props;
+
+  // console.log(dataUsers);
   return (
     <Layout pageTitle="User Page">
+      {dataUsers.map(user => {
+        return (
+          <>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </>
+        )
+      })}
       <p>UsersPage</p>
     </Layout>
   )
+}
+
+// Get Static Props
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const dataUsers = await res.json();
+
+  return {
+    props: {
+      dataUsers: dataUsers,
+    },
+  };
 }
